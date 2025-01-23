3dde25ba7668e53d6c49f4964c0933828db1b803
TheEnd-alr-taken <e0959517@u.nus.edu>
2025-01-23 21:54:30 +0800

    Complete Level-0: Rename, Greet, Exit

---
+++ src/main/java/Motiva.java
@@ -0,0 +1,16 @@
+public class Motiva {
+    public static void main(String[] args) {
+        String logo =
+            " __  __       _   _\n" +
+            "|  \\/  | ___ | |_(_)_   ____ _\n" +
+            "| |\\/| |/ _ \\| __| \\ \\ / / _` |\n" +
+            "| |  | | (_) | |_| |\\ V / (_| |\n" +
+            "|_|  |_|\\___/ \\__|_| \\_/ \\__,_|";
+        System.out.println(logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Motiva.\n What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
