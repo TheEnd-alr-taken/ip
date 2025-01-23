7451fd51f2002f95675f5734e323a48bbf0812ce
TheEnd-alr-taken <e0959517@u.nus.edu>
2025-01-23 22:07:58 +0800

    Complete Level-1: Echo

--- src/main/java/Motiva.java
+++ src/main/java/Motiva.java
@@ -1,16 +1,49 @@
+import java.util.Scanner;
+
 public class Motiva {
-    public static void main(String[] args) {
+
+    private static void formatReply(String text) {
+        String indent = " ".repeat(4);
+        String[] lines = text.split("\n");
+
+        System.out.println(indent + "============================================================");
+
+        for (String line : lines) {
+            System.out.println(" "+ indent + line);
+        }
+
+        System.out.println(indent + "============================================================\n");
+    }
+
+    private static void sayGreeting() {
         String logo =
             " __  __       _   _\n" +
             "|  \\/  | ___ | |_(_)_   ____ _\n" +
             "| |\\/| |/ _ \\| __| \\ \\ / / _` |\n" +
             "| |  | | (_) | |_| |\\ V / (_| |\n" +
-            "|_|  |_|\\___/ \\__|_| \\_/ \\__,_|";
-        System.out.println(logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Motiva.\n What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+            "|_|  |_|\\___/ \\__|_| \\_/ \\__,_|\n\n";
+
+        formatReply(logo + " Hello! I'm Motiva.\n What can I do for you?");
+    }
+
+    private static void sayGoodBye() {
+        formatReply(" Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        sayGreeting();
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = "";
+
+        while (true) {
+            userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                sayGoodBye();
+                break;
+            } else {
+                formatReply(userInput);
+            }
+        }
     }
 }
