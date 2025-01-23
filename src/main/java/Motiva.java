fc8d826ad4b291fffbe0b4f665d6c2fc4defccf3
TheEnd-alr-taken <e0959517@u.nus.edu>
2025-01-24 00:23:52 +0800

    Complete Level-3: Mark as Done

--- src/main/java/Motiva.java
+++ src/main/java/Motiva.java
@@ -31,16 +31,16 @@ public class Motiva {
         formatReply("Bye. Hope to see you again soon!");
     }
 
-    private static void listTasks(ArrayList<String> taskList) {
+    private static void listTasks(ArrayList<Task> taskList) {
 
         if (taskList.isEmpty()) {
             System.out.println("No tasks found.");
         } else {
-            String text = "";
+            String text = "Here are the tasks in your list:\n";
             int count = 1;
 
-            for (String task : taskList) {
-                text += count + ". " + task + "\n";
+            for (Task task : taskList) {
+                text += count + "." + task + "\n";
                 count++;
             }
 
@@ -48,8 +48,32 @@ public class Motiva {
         }
     }
 
-    private static void addTask(String userInput, ArrayList<String> taskList) {
-        taskList.add(userInput);
+    private static void toggleTask(String userInput, ArrayList<Task> taskList ) {
+        try {
+            String[] parts = userInput.split(" ");
+            int index = Integer.parseInt(parts[1]) - 1;
+            Task task = taskList.get(index);
+
+            if (userInput.startsWith("mark") && !task.isDone()) {
+                task.toggleDone();
+                formatReply("Nice! I've marked this task as done:\n  " + task);
+
+            } else if (userInput.startsWith("unmark") && task.isDone()) {
+                task.toggleDone();
+                formatReply("OK, I've marked this task as not done yet:\n  " + task);
+
+            } else {
+                formatReply("\"" + task + "\" is already " + parts[0] +"ed");
+            }
+
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            formatReply("Invalid index: no tasks found with that index");
+        }
+    }
+
+    private static void addTask(String userInput, ArrayList<Task> taskList) {
+        Task newTask = new Task(userInput);
+        taskList.add(newTask);
         formatReply("added: " + userInput);
     }
 
@@ -58,15 +82,23 @@ public class Motiva {
 
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
-        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
             userInput = scanner.nextLine();
+
+            if (userInput.isEmpty()) {
+                formatReply("No task captured.\nDo key in the task for me to keep track.");
+                continue;
+            }
+
             if (userInput.equals("bye")) {
                 sayGoodBye();
                 break;
             } else if (userInput.equals("list")) {
                 listTasks(taskList);
+            } else if (userInput.startsWith("mark") || userInput.startsWith("unmark")) {
+                toggleTask(userInput, taskList);
             } else {
                 addTask(userInput, taskList);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void toggleDone() {
+        this.isDone = !this.isDone;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + this.description;
+    }
+}
